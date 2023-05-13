145b1eb9105695015559727b3f55d2e2d16ff726
Rehman Hesenov <Ms.rehman@mail.ru>
2023-05-13 17:45:56 +0400

    numune dosya

---
+++ assets/JS/script.js
@@ -0,0 +1,30 @@
+// function proses() {
+//     alert("salam")
+// }
+
+
+// addEventListener("click", () =>{
+//     alert("ok")
+// }
+// )
+
+
+// const a=document.getElementById("a")
+// const b=document.getElementById("b")
+// const cem=document.getElementById("cem")
+
+// function proses() {
+//    cem.innerHTML=Number(a.value)+Number(b.value)
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
